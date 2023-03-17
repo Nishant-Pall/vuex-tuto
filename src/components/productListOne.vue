453264a01fb28c3dceef6edabdf38df7f8eb252b
prhyme <palln6935@gmail.com>
2023-03-17 13:02:11 +0530

    Consume getter in components

--- src/components/productListOne.vue
+++ src/components/productListOne.vue
@@ -2,7 +2,7 @@
 	<div id="product-list-one">
 		<h2>Product list one</h2>
 		<ul>
-			<li v-for="(product, index) in products" v-bind:key="index">
+			<li v-for="(product, index) in saleProducts" v-bind:key="index">
 				<span class="name">{{ product.name }}</span>
 				<span class="price">${{ product.price }}</span>
 			</li>
@@ -15,6 +15,9 @@ export default {
 	computed: {
 		products() {
 			return this.$store.state.products;
+		},
+		saleProducts() {
+			return this.$store.getters.saleProducts;
 		},
 	},
 };
--- src/components/productListTwo.vue
+++ src/components/productListTwo.vue
@@ -2,7 +2,7 @@
 	<div id="product-list-two">
 		<h2>Product list two</h2>
 		<ul>
-			<li v-for="(product, index) in products" v-bind:key="index">
+			<li v-for="(product, index) in saleProducts" v-bind:key="index">
 				<span class="name">{{ product.name }}</span>
 				<span class="price">${{ product.price }}</span>
 			</li>
@@ -15,6 +15,9 @@ export default {
 	computed: {
 		products() {
 			return this.$store.state.products;
+		},
+		saleProducts() {
+			return this.$store.getters.saleProducts;
 		},
 	},
 };
